d9e927c21479f9d34f2af2dbf82379721e78b3d7
root <root@localhost.localdomain>
2017-10-11 23:12:10 +0300

    new lesson

---
+++ lesson3.php
@@ -0,0 +1,51 @@
+<?php
+
+require ('header.html');
+
+?>
+
+<div id="content-container">
+		<div class="avatar">
+			<img src="img/user.png" /> 
+		</div>
+		<div class="username">
+			Semenov Oleg
+		</div>
+		<div class="time">
+			два часа назад
+		</div>
+		<div class="content">
+			<img src="img/foto1.jpeg" />
+		</div>
+		<div class="likes">
+			<img src="img/like.png"/> 55
+		</div>
+		<div class="comment">
+			Море, солнце, небо
+		</div>
+	
+	
+		<div class="avatar">
+			<img src="img/user.png" width="40" height="40" /> 
+		</div>
+		<div class="username">
+			Alex007
+		</div>
+		<div class="time">
+			сутки назад
+		</div>
+		<div class="content">
+			<img src="img/foto2.jpeg" width="400" />
+		</div>
+		<div class="likes">
+			<img src="img/like.png" wigth="20" height="20"/> 55
+		</div>
+		<div class="comment">
+			Волны, и еще немного текста чтобы проверить переносится ли он на вторую строку 
+		</div>
+	</div>
+<?php
+
+require ('footer.php')
+
+?>
